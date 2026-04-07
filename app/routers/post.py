65d5e007a68745bb671336352e763f4f376c3ecd
avishkar <gawareavishkar2004@gmail.com>
2026-04-07 19:36:23 +0530

    include vote count in get single post using join

--- app/routers/post.py
+++ app/routers/post.py
@@ -26,7 +26,7 @@ def get_posts(db: Session = Depends(get_db),
     print(search)
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
-    
+
     # posts = db.query(models.Post).filter(
     #     models.Post.title.contains(search)).limit(Limit).offset(skip).all()
     
@@ -64,12 +64,15 @@ def create_posts(
 
 
 # GET INDIVIDUAL POST
-@router.get("/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.PostOut)
 def get_post(id: int, db: Session = Depends(get_db), 
     current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM posts where id = %s""",str(id))
     # post = cursor.fetchone()
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+    #post = db.query(models.Post).filter(models.Post.id == id).first()
+
+    post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
 
     if not post:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
